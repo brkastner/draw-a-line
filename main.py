eccc8f56c1118dd32efc942a40f5840339052cdc
kas <code@kasthe.dev>
2026-07-30 15:50:44 -0500

    feat: dynamic chart sizing and improved edge case handling
    - proper fix for negative slope handling
    - dynamically grow chart to support points outside the standard 10x10
      grid
    - added x and y axis labels
    - a few new tests to cover those edge cases

--- main.py
+++ main.py
@@ -7,19 +7,20 @@ import click
 def _calculate_all_points_in_line(x1: int, y1: int, x2: int, y2: int) -> defaultdict[int, list]:
     points: defaultdict[int, list] = defaultdict(list)
     # special handling for vertical line
-    if (start_x := min(x1, x2)) == (end_x := max(x1, x2)):
-        for y in range(min(y2, y1), max(y2, y1) + 1):
-            points[y].append(start_x)
+    if x1 == x2:
+        for y in range(min(y1, y2), max(y1, y2) + 1):
+            points[y].append(x1)
         return points
 
-    # solve for y = mx + b
-    slope = (y2 - y1) / (end_x - start_x)
-    b = int(round(y1 - slope * x1, 0))
+    # swap coordinates for negative slopes
+    if x1 > x2:
+        x1, y1, x2, y2 = x2, y2, x1, y1
+    slope = (y2 - y1) / (x2 - x1)
 
-    current_x = start_x
-    while current_x <= end_x:
-        points[int(round(slope * current_x, 0) + b)].append(current_x)
-        current_x += 1
+    # calculate all points in line (rounding to nearest int)
+    for x in range(x1, x2 + 1):
+        points[round(y1 + slope * (x - x1))].append(x)
+
     return points
 
 
@@ -35,21 +36,26 @@ def _generate_graph(point1: str, point2: str):
         click.echo('invalid inputs! please use the format x,y x,y')
         return
 
+    chart_x_start = min(x1, x2, 0)
+    chart_y_end = min(y1, y2, 0)  # y is printed in reverse
     max_x = max(x1, x2)
     max_y = max(y1, y2)
     points = _calculate_all_points_in_line(x1, y1, x2, y2)
     y_iter = max(max_y, 10)
-    while y_iter >= 0:
+    for y_iter in range(max(max_y, 10), chart_y_end - 1, -1):
+        click.echo(f"{y_iter:2d} ", nl=False)
         # loop through each row
         x_points = points.get(y_iter, set())
-        x_iter = 0
-        while x_iter <= max(max_x, 10):
-            click.echo(" ● " if x_iter in x_points else " ○ ", nl=False)
-            x_iter += 1
+        for x in range(chart_x_start, max(max_x, 10) + 1):
+            click.echo(" ● " if x in x_points else " ○ ", nl=False)
 
         # newline
         click.echo("")
-        y_iter -= 1
+
+    click.echo("   ", nl=False)
+    for x in range(chart_x_start, max(max_x, 10) + 1):
+        click.echo(f"{x:2d} ", nl=False)
+    click.echo("\n")
 
 
 @click.command()
@@ -65,11 +71,13 @@ TESTS = [
     ("1,1", "7,2"),
     ("1,1", "8,1"),
     ("1,1", "1,10"),
-    ("10,3", "2,6")
+    ("10,3", "2,6"),
+    ("1,10", "8,3"),
+    ("-1,-1", "12,12")
 ]
 
 
 if __name__ == '__main__':
     for xpos, ypos in TESTS:
-        click.echo(f'({xpos}) -> ({ypos})')
+        click.echo(f'({xpos}) -> ({ypos})\n')
         _generate_graph(xpos, ypos)
